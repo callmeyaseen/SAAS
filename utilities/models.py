35ea353e6aaed54bf1dd01c1b0a8d99b07ed1e59
callmeyaseen <my1476662@gmail.com>
2026-04-04 07:36:10 -0700

    Added created_at field in Rack model

--- utilities/models.py
+++ utilities/models.py
@@ -22,6 +22,7 @@ class Vendor(models.Model):
 class Rack(models.Model):
     rack_no = models.CharField(max_length=20)
     location = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return self.rack_no
 
